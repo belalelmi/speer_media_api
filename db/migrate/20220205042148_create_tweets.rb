23be670a82299959a5c0e268d20879b352171ff7
belalelmi <belalelmi@gmail.com>
2022-02-05 08:23:32 -0500

    fixed migtration issues causing incorrect user validation

--- db/migrate/20220205042148_create_tweets.rb
+++ db/migrate/20220205042148_create_tweets.rb
@@ -1,9 +1,12 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-      t.text :tweet
+      t.string :tweet, null: false
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
+
+      t.index [:user_id, :created_at]
     end
   end
 end
