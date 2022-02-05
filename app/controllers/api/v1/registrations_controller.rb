9207769c30201f7ee80485aab89243470f359b12
belalelmi <belalelmi@gmail.com>
2022-02-05 10:10:29 -0500

    fixed json response

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::RegistrationsController < ApplicationController
         user: user
       }
     else
-        render json: { status: 500 }
+        render json: { status: 401 }
     end
   end
 end
