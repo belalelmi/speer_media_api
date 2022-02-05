3d0438bb8f934e76e6da6a7c5810e31056689a2a
belalelmi <belalelmi@gmail.com>
2022-02-04 23:28:49 -0500

    Scaffold Tweet

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :registrations, only: [:create]
       delete :logout, to: "sessions#logout"
       get :logged_in, to: "sessions#logged_in"
+      resources :tweets
       root to: "application#home"
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_020254) do
+ActiveRecord::Schema.define(version: 2022_02_05_042148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2022_02_05_020254) do
     t.string "name"
     t.integer "age"
     t.datetime "born_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.text "tweet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
