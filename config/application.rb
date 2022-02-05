327aa60e66ec1993e5e9092cbc70d7b11c153a1c
belalelmi <belalelmi@gmail.com>
2022-02-04 20:56:17 -0500

    setup for RSpec testing

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,13 @@ module SpeerMediaApi
     # the framework and any gems in your application.
     config.generators.system_tests = nil
     config.generators do |g|
-      g.test_framework = :rspec
+      g.test_framework(
+        :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+      )
     end 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
-  delete :logout, to: "sessions#logout"
-  get :logged_in, to: "sessions#logged_in"
-  root to: "application#home"
+  namespace :api do
+    defaults format: :json do
+      resources :sessions, only: [:create]
+      resources :registrations, only: [:create]
+      delete :logout, to: "sessions#logout"
+      get :logged_in, to: "sessions#logged_in"
+      root to: "application#home"
+    end
+  end
 end
