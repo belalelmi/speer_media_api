166742885d3218b2395788abd0236dce75062360
belalelmi <belalelmi@gmail.com>
2022-02-04 23:11:39 -0500

    added RSpec tests for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  # validates_presence_of :email
+  # validates_uniqueness_of :email
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+
 end
