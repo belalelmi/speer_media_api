11471a872257d4551c2bc3a7bc5a036e5ff22fb9
belalelmi <belalelmi@gmail.com>
2022-02-05 12:10:21 -0500

    fixed session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -28,22 +28,4 @@ module SessionsHelper
   def current_user?(user)
     user && user == current_user
   end
-
-  def logged_in_user
-    unless logged_in?
-      redirect_to login_path, notice: "Please sign in."
-    end
-  end
-
-  def remember(user)
-    user.remember
-    cookies.permanent.encrypted[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
 end
